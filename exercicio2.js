5960a5bf1d28126b33605db9c47e03180ec918d3
Diogo Paza <diogopazacvel@gmail.com>
2017-12-25 15:49:12 -0200

    segundo exercicio

--- exercicio2.js
+++ exercicio2.js
@@ -1,4 +1,18 @@
 //console.log(process.argv)
+let tot =0;
+const args = process.argv.slice(2)
+//let pos1 = process.argv[2];
+//console.log(pos1)
 
-let result = process.argv;
-console.log(result);
+
+for(i=0;i<args.length;i++){
+    valor = Number( args[i] )
+    tot += valor; 
+    
+
+}
+
+console.log(tot)
+
+//let result = process.argv;
+//console.log(result);
---
+++ testes.js
@@ -0,0 +1,8 @@
+let args = process.argv.slice(2)
+let result = 0;
+
+for(let i of args){
+
+    result += Number(i)
+}
+
